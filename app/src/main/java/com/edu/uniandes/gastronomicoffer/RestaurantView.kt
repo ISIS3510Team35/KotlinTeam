a89d883119fdb10f3f054fd0319926d430da8024
Juan Pablo Martinez Pineda <jp.martinezp1@uniandes.edu.co>
2023-09-23 04:34:10 -0500

    RestaurantView completed

---
+++ app/src/main/java/com/edu/uniandes/gastronomicoffer/RestaurantView.kt
@@ -0,0 +1,12 @@
+package com.edu.uniandes.gastronomicoffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edu.uniandes.kotlinteam.R
+
+class RestaurantView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.restaurant_view)
+    }
+}
