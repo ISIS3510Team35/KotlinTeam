78e7038a2f9757fe288f768c6717a290d3462c78
Juan Pablo Martinez Pineda <jp.martinezp1@uniandes.edu.co>
2023-11-03 02:23:27 -0500

    Restaurant detail and fix in UI

--- app/src/main/java/com/edu/uniandes/fud/RestaurantActivity.kt
+++ app/src/main/java/com/edu/uniandes/fud/RestaurantActivity.kt
@@ -3,16 +3,24 @@ package com.edu.uniandes.fud
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import com.edu.uniandes.fud.ui.restaurant.RestaurantScreen
 import com.edu.uniandes.fud.ui.theme.MobileAppTheme
+import com.edu.uniandes.fud.viewModel.restaurant.RestaurantViewModel
+import com.edu.uniandes.fud.viewModel.restaurant.RestaurantViewModelFactory
 
 class RestaurantActivity : ComponentActivity() {
 
+    private val restaurantViewModel: RestaurantViewModel by viewModels {
+        RestaurantViewModelFactory((application as FuDApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        restaurantViewModel.setInitialRestaurant(this.intent.getStringExtra("restaurantId").orEmpty().toInt())
         setContent {
             MobileAppTheme {
-                RestaurantScreen()
+                RestaurantScreen(restaurantViewModel)
             }
         }
     }
---
+++ app/src/main/java/com/edu/uniandes/fud/viewModel/restaurant/RestaurantViewModel.kt
@@ -0,0 +1,44 @@
+package com.edu.uniandes.fud.viewModel.restaurant
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.edu.uniandes.fud.domain.RestaurantProduct
+import com.edu.uniandes.fud.repository.DBRepository
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class RestaurantViewModel(repository: DBRepository) : ViewModel() {
+
+    private val _restaurant = MutableLiveData<RestaurantProduct>()
+    val restaurant: LiveData<RestaurantProduct> = _restaurant
+
+    private val _restaurantId = MutableLiveData<Int>()
+
+    val dbRepository = repository
+
+    fun setInitialRestaurant(restaurantId: Int) {
+        _restaurantId.value = restaurantId
+    }
+
+    init {
+        viewModelScope.launch {
+            repository.restaurantsProducts.collect() { restaurantProducts ->
+                _restaurant.value = restaurantProducts.find { rest -> _restaurantId.value == rest.id }
+            }
+        }
+    }
+
+}
+
+class RestaurantViewModelFactory(private val repository: DBRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RestaurantViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return RestaurantViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
