a67930f12d334f57bdfe9491d85db96de1f7bee2
danielfgmb <49533662+danielfgmb@users.noreply.github.com>
2023-11-06 16:25:36 -0500

    Image Caching Policies

--- app/src/main/java/com/edu/uniandes/fud/FuDApplication.kt
+++ app/src/main/java/com/edu/uniandes/fud/FuDApplication.kt
@@ -1,14 +1,40 @@
 package com.edu.uniandes.fud
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import com.edu.uniandes.fud.database.DatabaseRoom
 import com.edu.uniandes.fud.repository.DBRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
-class FuDApplication : Application() {
+class FuDApplication : Application(), ImageLoaderFactory {
     val applicationScope = CoroutineScope(SupervisorJob())
     val database by lazy { DatabaseRoom.getDataBase(this, applicationScope) }
     val repository by lazy { DBRepository(database) }
-    
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this).newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.1)
+                    .weakReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
+    }
+
 }
